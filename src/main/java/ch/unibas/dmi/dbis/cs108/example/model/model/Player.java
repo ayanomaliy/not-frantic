02f6b99f394139301811d8f44c680653871020e1
Denys Verich <denisverich@gmail.com>
2026-03-04 15:24:09 +0100

    fixed missing javadoc in player.java

--- src/main/java/ch/unibas/dmi/dbis/cs108/example/model/model/Player.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/example/model/model/Player.java
@@ -1,4 +1,14 @@
 package ch.unibas.dmi.dbis.cs108.example.model.model;
 
+/**
+ * Represents a player in the game.
+ * <p>
+ * This record encapsulates the basic information of a player, including their name.
+ * Instances of this record are immutable and used throughout the server for managing
+ * player state and communicating player information to clients.
+ * </p>
+ *
+ * @param name the name of the player
+ */
 public record Player(String name) {
 }
